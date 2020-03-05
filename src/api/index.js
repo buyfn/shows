96842f94aa8ea472aa373311cfd6402ba96b49f1
Ihnat Piatrenka <ihnat_piatrenka@epam.com>
2020-03-05 16:48:51 +0300

    Use https

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const getShowData = showID =>
-  axios.get(`http://api.tvmaze.com/shows/${showID}`);
+  axios.get(`https://api.tvmaze.com/shows/${showID}`);
 
 export const getShowEpisodes = showID =>
-  axios.get(`http://api.tvmaze.com/shows/${showID}/episodes`);
+  axios.get(`https://api.tvmaze.com/shows/${showID}/episodes`);
 
 export const getEpisodeData = episodeID =>
-  axios.get(`http://api.tvmaze.com/episodes/${episodeID}`);
+  axios.get(`https://api.tvmaze.com/episodes/${episodeID}`);
